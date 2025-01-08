aced2f403d935657399ba346912368b0bb48d2b1
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-01-07 21:47:31 -0500

    Se modifico por ultima vez el xaml, ademas de cambiarse un error que habia en la pagina principal .xaml.cs

--- People/MainPage.xaml.cs
+++ People/MainPage.xaml.cs
@@ -1,31 +1,32 @@
 ﻿using People.Models;
+using People.ViewModels;
 using System.Collections.Generic;
 
 namespace People;
 
 public partial class MainPage : ContentPage
 {
+    public mathiasveraPaginaPrincipalViewModel ViewModel { get; }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        ViewModel = new mathiasveraPaginaPrincipalViewModel();
+        BindingContext = ViewModel;
+    }
 
     public void OnNewButtonClicked(object sender, EventArgs args)
     {
-        statusMessage.Text = "";
+        ViewModel.StatusMessage = "";
 
-        App.PersonRepo.AddNewPerson(newPerson.Text);
-        statusMessage.Text = App.PersonRepo.StatusMessage;
+        ViewModel.AddPersonCommand.Execute(null);
     }
 
     public void OnGetButtonClicked(object sender, EventArgs args)
     {
-        statusMessage.Text = "";
+        ViewModel.StatusMessage = "";
 
-        List<Person> people = App.PersonRepo.GetAllPeople();
-        peopleList.ItemsSource = people;
+        ViewModel.GetPeopleCommand.Execute(null);
     }
-
 }
 
